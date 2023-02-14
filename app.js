cf870ed6570968ecd67c87c4dbffab76c7eca75c
HamzaSMIT <creatorbro135@gmail.com>
2023-02-14 23:19:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,475 @@
+
+                                        // Chapter:01
+
+// Question:01
+
+// alert ("Hello World")
+
+// Question:02
+
+// alert ("Error! Please enter a valid password.")
+
+// Question:03
+
+// alert ("Welcome to JS Land... \nHappy Coding!")
+
+// Question:04
+
+// alert ("Welcome to JS Land...")
+// alert ("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+// Question:05
+
+// alert ("Hello...I can run JS through my web browser's console")
+
+
+                                        // Chapter:02
+
+// Question:01
+
+// var username;
+
+// Question:02
+
+// var myName= "M.Hamza"
+
+// Question:03
+
+// var message= "Hello World"
+// alert (message);
+
+// Question:04
+
+// var fullName = "Jhone Doe";
+// var age = "15 years old";
+// var work = "Certified Mobile Application Development";
+
+// alert(fullName);
+// alert(age);
+// alert( work );
+
+// Question:05
+
+// alert ("PIZZA \nPIZZ \nPIZ \nPI \nP")
+
+// Question:06
+
+// var email="creatorbro135@gmail.com"
+// alert ("My email address is " + email)
+
+// Question:07
+
+// var book= "A smarter way to learn JavaScript"
+// alert ("I am trying to learn from the Book " + book)
+
+// Question:08
+
+// document.write ("Yah! I can write HTML content through JavaScript")
+
+// Question:09
+
+// alert("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+            // and
+// document.write("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬")
+
+                                        // Chapter:03
+ 
+// Question:01
+
+// var age= 15
+// alert("I am " + age + " years old")
+                                        
+// Question:02
+
+// Pending
+
+// Question:03
+
+// var birthYear= 1990
+// document.write("My birth year is " + birthYear + "<br>");
+// document.write("Data type of my declared variable is number");
+
+// Question:04
+
+// let visitorName = prompt("Enter your Full Name:");
+// console.log("Visitor's Name: " + visitorName);
+
+// let productTitle = prompt("Enter the product title:");
+// console.log("Product Title: " + productTitle);
+
+// let quantity = +prompt("Enter the quantity you want to order:");
+// console.log("Quantity: " + quantity);
+
+// document.write ( visitorName + " orderd" +" "+ quantity  +" "+ productTitle  + " on XYZ Clothing Store")
+
+                                        // Chapter:04
+
+// Question:01
+
+// let a=5, b="Hello World", c=8;
+
+// Question:02
+
+// Legal Variables
+// let firstName= "Muhammad Hamza"
+// let _numbers= 123
+// let $dollar= 267
+// let camelCase= "2nd word 1st letter capital"
+// let PascalCase= "1st word 1st letter capital & 2nd word 1st letter capital"
+
+// Illegal Variables
+// let 778number =123445;
+// let first-name = "Muhammad Hamza";
+// let if = "age20"
+// let ifelse= "20"
+// let elseif = "wrongway"
+
+// Question:03
+
+// document.write("Rules for naming JS variables" + "<br>")
+
+// document.write("<br>")
+// document.write("<br>")
+// document.write("<br>")
+
+
+// document.write("A heading stating “Rules for naming JS variables”"+ "<br>")
+// document.write("Variable names can only contain ______, ______, ______ and ______." + "<br>")
+// document.write("For Example $my_1stVariable")
+// document.write(" " + "<br>")
+// document.write("Variables must begin with a ______, ______ or _____." + "<br>")
+// document.write("For example $name, _name or name" + "<br>")
+// document.write("Variable names are case _________" + "<br>")
+// document.write("Variable names should not be JS _________" )
+
+                                        // Chapter:05
+
+// Question:01                                       
+
+// let a=3
+// let b=5
+// console.log( a + b)
+
+// document.write("Sum of " + a + " and " + b + " is " + (a+b))
+
+// Question:02
+
+// Subtraction
+// let a=3
+// let b=5
+// console.log( a - b)
+
+// document.write("Subtraction of " + a + " and " + b + " is " + (a-b))
+
+// Multiplication
+// let a=3
+// let b=5
+// console.log( a * b)
+
+// document.write("Multiplication of " + a + " and " + b + " is " + (a*b))
+
+// Division
+// let a=3
+// let b=5
+// console.log( a / b)
+
+// document.write("Division of " + a + " and " + b + " is " + (a/b))
+
+// Modulus
+// let a=3
+// let b=5
+// console.log( a % b)
+
+// document.write("Modulus of " + a + " and " + b + " is " + (a%b))
+
+// Question:03
+
+// a
+// var fullName= "Muhammad Hamza"
+
+// b 
+// document.write("Value after variable declaration is " + fullName)
+
+// c 
+// var init= 6
+
+// d 
+// document.write("Initial value " + init)
+
+// e 
+// let a= 10;
+// console.log( a++)
+
+// f 
+// document.write("Value after increment is " + (a++))
+
+// g 
+// let a= 10;
+// console.log( ++a + 7 )
+
+// h 
+// document.write("Value after addition is " + (++a + 7))
+
+// i 
+// let a= 10 
+// console.log(a--)
+
+// j 
+// document.write("Value after decrement is " + (a--))
+
+// k 
+// let a= 10 
+// console.log(a--)
+// let remainder= a % 3;
+
+// l 
+// document.write("The remainder is " + remainder)
+
+// Question:04
+
+// let value= 600
+// let value2= 5
+// console.log( value * value2 )
+
+// document.write("Total cost to buy " + value2 + " tickets to a movie is " + (value*value2) + "PKR")
+
+// Question:05
+
+// let four= 4
+// let four1= 1
+// let four2= 2
+// let four3= 3
+// let four4= 4
+// let four5= 5
+// let four6= 6
+// let four7= 7
+// let four8= 8
+// let four9= 9
+// let four10= 10
+
+// console.log(four*four1)
+// console.log(four*four2)
+// console.log(four*four3)
+// console.log(four*four4)
+// console.log(four*four5)
+// console.log(four*four6)
+// console.log(four*four7)
+// console.log(four*four8)
+// console.log(four*four9)
+// console.log(four*four10)
+
+// document.write("Table of 4" + "<br>")
+// document.write("<br>")
+// document.write("4 x 1 =" + (four*four1) + "<br>")
+// document.write("4 x 2 =" + (four*four2) + "<br>")
+// document.write("4 x 3 =" + (four*four3) + "<br>")
+// document.write("4 x 4 =" + (four*four4) + "<br>")
+// document.write("4 x 5 =" + (four*four5) + "<br>")
+// document.write("4 x 6 =" + (four*four6) + "<br>")
+// document.write("4 x 7 =" + (four*four7) + "<br>")
+// document.write("4 x 8 =" + (four*four8) + "<br>")
+// document.write("4 x 9 =" + (four*four9) + "<br>")
+// document.write("4 x 10 =" + (four*four10) + "<br>")
+
+// Question:06
+
+// a 
+//  var celcius= 25
+// //  b 
+//  var farenheit=(celcius * 9/5) + 32;
+//  document.write(celcius + "°C is " + farenheit + "°F")
+// c 
+//  var farenheit= 70
+// //  d 
+//  var celcius=(farenheit - 32) * 5/9;
+//  document.write(farenheit + "°F is " + celcius + "°C")
+
+// Question:07
+
+// // a 
+// let item1Price= 650
+// // b 
+// let item2Price= 100
+// // c 
+// let item1Quantity= 3
+// // d 
+// let item2Quantity= 7
+// let shipCharges= 100
+
+// let totalCost=( item1Price * item1Quantity + item2Price * item2Quantity + shipCharges )
+
+// document.write( "Price of item 1 is " + item1Price + "<br>")
+// document.write( "Quantity of item 1 is " + item1Quantity + "<br>")
+// document.write( "Price of item 2 is " + item2Price + "<br>")
+// document.write( "Quantity of item 2 is " + item2Quantity + "<br>")
+// document.write( "Shipping Charges " + shipCharges + "<br>")
+// document.write("<br>")
+// document.write("<br>")
+// document.write("Total cost of your order is " + totalCost )
+
+// Question:08
+
+// let totalMarks= 980
+// let marksObtained=804
+// let percentage=( marksObtained / totalMarks ) * 100 ;
+
+// document.write("Total Marks: " + totalMarks + "<br>")
+// document.write("Marks Obtained: " + marksObtained + "<br>")
+// document.write("Total Marks: " + percentage + "%")
+
+// Question:09
+
+// let usDollar=104.80
+// let saudiRiyal=28
+// let dollarQuantity=10
+// let riyalQuantity=25
+// let totalPKR= (usDollar * dollarQuantity + saudiRiyal * riyalQuantity)
+
+// document.write("Total Currency in PKR: " + totalPKR)
+
+// Question:10
+
+// let a=5
+// let result= (a + 5) * 10 / 2;
+// console.log(result)
+
+// Question:11
+
+// let currentYear=2016
+// let birthYear=1992
+// let age=(2016-1992)
+// let age2= (age - 1)
+
+// document.write("Current Year: " + currentYear + "<br>")
+// document.write("Birth Year: " + birthYear + "<br>")
+// document.write("Your Age is: " + age + "<br>")
+// document.write("<br>")
+// document.write("They are either " + age + " or " + age2 + " years old")
+
+// Question:12
+
+// let radius=20
+// let π=3.142
+// let circum= 2 * π * radius;
+// console.log(circum)
+// let area= π * radius ** 2;
+// console.log(area)
+
+// document.write("Radius of a circle: " + radius + "<br>")
+// document.write("The Circumference is: " + circum + "<br>")
+// document.write("The area is: " + area ) 
+
+// Question:13
+
+// var favSnack= "Doritos"
+// var age=15
+// var maxAge=65
+// var amountPday=3
+// var total=(maxAge-age)*amountPday;
+// console.log(total)
+
+// document.write("Favourite Snack: " + favSnack + "<br>")
+// document.write("Current age: " + age + "<br>")
+// document.write("Estimated Maximum Age: " + maxAge + "<br>")
+// document.write("Amount of snacks per day: " + amountPday + "<br>")
+// document.write("You will need " + total + " " + favSnack + " to last you untill the ripe old age of " + maxAge + "<br>")
+
+                                        // Chapter:06 to 09
+
+// Question:01
+
+// let a=10
+// let value1=(++a)
+// let value2=(a++)
+// let value3=(--a)
+// let value4=(a--)
+
+
+// document.write("The value of a is: " + a)
+// document.write("<br>")
+// document.write("----------------------------------------")
+// document.write("<br>")
+
+// document.write("The value of ++a is: " + value1 + "<br>")
+// document.write("Now the value of a is: " + value1)
+// document.write("<br>")
+// document.write("<br>")
+
+// document.write("The value of a++ is: " + value2 + "<br>")
+// document.write("Now the value of a is: " + "12")
+// document.write("<br>")
+// document.write("<br>")
+
+// document.write("The value of --a is: " + value3 + "<br>")
+// document.write("Now the value of a is: " + value3)
+// document.write("<br>")
+// document.write("<br>")
+
+// document.write("The value of a-- is: " + value4 + "<br>")
+// document.write("Now the value of a is: " + "10")
+// document.write("<br>")
+// document.write("<br>")
+
+// Question:02
+
+// var a = 2, b = 1;
+             //  1      0    1      1    =3
+// var result = --a - --b + ++b + b--;
+// console.log(result)
+            //  1
+// var result1= --a; 
+// console.log(result1)
+          //   1     0
+// var result2= --a - --b;
+// console.log(result2)
+            //   1     0     1    = 2
+// var result3= --a - --b + ++b;
+// console.log(result3)
+            //   1     0     1     1   =3
+// var result4= --a - --b + ++b + b--;
+// console.log(result4)
+
+// document.write("a is: " + a + "<br>")
+// document.write("b is: " + b + "<br>")
+// document.write("result is: " + result )
+
+// Question:03 and 04
+
+// var userName= prompt("Enter your Name")
+// alert("Welcome to JS Land...")
+
+// Question:05
+
+// var num = +prompt("Enter a number:");
+
+// let fifth=5
+// let one=1
+// let two=2
+// let three=3
+// let four=4
+// let five=5
+// let six=6
+// let seven=7
+// let eight=8
+// let nine=9
+// let ten=10
+
+// document.write( "5 x 1 = " + (fifth*one) + "<br>" )
+// document.write( "5 x 2 = " + (fifth*two) + "<br>" )
+// document.write( "5 x 3 = " + (fifth*three) + "<br>" )
+// document.write( "5 x 4 = " + (fifth*four) + "<br>" )
+// document.write( "5 x 5 = " + (fifth*five) + "<br>")
+// document.write( "5 x 6 = " + (fifth*six) + "<br>" )
+// document.write( "5 x 7 = " + (fifth*seven) + "<br>")
+// document.write( "5 x 8 = " + (fifth*eight) + "<br>")
+// document.write( "5 x 9 = " + (fifth*nine) + "<br>")
+// document.write( "5 x 10 = " + (fifth*ten) + "<br>" )
+
+// Question:06
+
+// var sub1="English"
+// var sub2="Urdu"
+// var sub3="Maths"
+// var totalMarks= 100
+
+
